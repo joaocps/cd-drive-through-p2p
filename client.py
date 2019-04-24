8bc998f13fc1e7aaef8622ec06460a567c998288
JoãoSantos <jcps@ua.pt>
2019-04-24 15:43:20 +0100

    Professor client changes not upstream

--- client.py
+++ client.py
@@ -6,7 +6,6 @@ import socket
 import random
 import logging
 import argparse
- 
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
@@ -16,11 +15,17 @@ logging.basicConfig(level=logging.DEBUG,
 def main(port, ring, timeout):
     # Create a logger for the client
     logger = logging.getLogger('Client')
-    
+
     # UDP Socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(timeout)
     sock.bind(('localhost', port))
+
+    # Generate a request
+    order = {'hamburger': 0, 'fries': 0, 'drink': 0}
+    quantity = random.randint(1, 5)
+    for i in range(quantity):
+        order[random.choice(['hamburger', 'fries', 'drink'])] += 1
 
     # Wait for a random time
     delta = random.gauss(2, 0.5)
@@ -28,19 +33,18 @@ def main(port, ring, timeout):
     time.sleep(delta)
 
     # Request some food
-    logger.info('Request some food...')
-    p = pickle.dumps({'method': 'ORDER', 'args': {'hamburger': 1}})
+    logger.info('Request some food: %s', order)
+    p = pickle.dumps({'method': 'ORDER', 'args': order})
     sock.sendto(p, ring)
 
     # Wait for Ticket
     p, addr = sock.recvfrom(1024)
     o = pickle.loads(p)
-    #print(o)
     logger.info('Received ticket %s', o['args'])
 
-    # Pickup order 
+    # Pickup order
     logger.info('Pickup order %s', o['args'])
-    p = pickle.dumps({"method": 'PICKUP', "args": o['args']})
+    p = pickle.dumps({'method': 'PICKUP', 'args': o['args']})
     sock.sendto(p, ring)
 
     # Wait for order
@@ -58,6 +62,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Pi HTTP server')
     parser.add_argument('-p', dest='port', type=int, help='client port', default=5004)
     parser.add_argument('-r', dest='ring', type=int, help='ring ports ', default=5000)
-    parser.add_argument('-t', dest='timeout', type=int, help='socket timeout', default=30)
+    parser.add_argument('-t', dest='timeout', type=int, help='socket timeout', default=90)
     args = parser.parse_args()
-    main(args.port, ('localhost', args.ring), args.timeout)
+main(args.port, ('localhost', args.ring), args.timeout)
